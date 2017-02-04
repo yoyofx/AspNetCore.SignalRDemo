39214062d0e46d5bc71728d23f11aa00b4d8c378
张磊 <maxzhang1985@me.com>
2017-02-04 11:50:25 +0800

    add files

---
+++ src/WebApplication7/Hubs/Chathub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication7.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static List<string> ConnectedUsers;
+
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+
+        public void Connect(string newUser)
+        {
+            if (ConnectedUsers == null)
+                ConnectedUsers = new List<string>();
+
+            ConnectedUsers.Add(newUser);
+            Clients.Caller.getConnectedUsers(ConnectedUsers);
+            Clients.Others.newUserAdded(newUser);
+        }
+    }
+}
---
+++ src/WebApplication7/wwwroot/chat.js
@@ -0,0 +1,36 @@
+﻿var userName = prompt("Enter your name: ");
+var chat = $.connection.chatHub;
+chat.client.messageReceived = function (originatorUser, message) {
+    $("#messages").append('<li><strong>' + originatorUser + '</strong>: ' + message);
+};
+
+chat.client.getConnectedUsers = function (userList) {
+    for (var i = 0; i < userList.length; i++)
+        addUser(userList[i]);
+};
+
+chat.client.newUserAdded = function (newUser) {
+    addUser(newUser);
+}
+
+$("#messageBox").focus();
+
+$("#sendMessage").click(function () {
+    chat.server.send(userName, $("#messageBox").val());
+    $("#messageBox").val("");
+    $("#messageBox").focus();
+});
+
+$("#messageBox").keyup(function (event) {
+    if (event.keyCode == 13)
+        $("#sendMessage").click();
+});
+
+function addUser(user) {
+    $("#userList").append('<li>' + user + '</li>');
+}
+
+$.connection.hub.logging = true;
+$.connection.hub.start().done(function () {
+    chat.server.connect(userName);
+});
