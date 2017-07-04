e2083f9dc6e06ce685aadac5b5b62ca0a4a508a6
max zhang <zl.hxd@hotmail.com>
2017-07-04 14:28:34 +0800

    2.0
    2.0

---
+++ SignalR_AspNetCore2.0Preview/SignalR_AspNetCoreDemo2/Hubs/Chat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication2.Hubs
+{
+    public class Chat : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} joined");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} left");
+        }
+
+        public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("Send", $"{Context.ConnectionId}: {message}");
+        }
+
+        public Task SendToGroup(string groupName, string message)
+        {
+            return Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} joined {groupName}");
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} left {groupName}");
+        }
+
+        public Task Echo(string message)
+        {
+            return Clients.Client(Context.ConnectionId).InvokeAsync("Send", $"{Context.ConnectionId}: {message}");
+        }
+    }
+}
